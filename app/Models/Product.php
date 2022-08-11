d2ff2f2ee9f2ae46248522f7741ea8b389d084be
Chico Freitas <freitas.phys84@gmail.com>
2022-08-11 17:32:00 -0300

    set relations btw company and produc: one to many

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -8,4 +8,12 @@ use Illuminate\Database\Eloquent\Model;
 class Product extends Model
 {
     use HasFactory;
+
+    /**
+     * Get the post that owns the comment.
+     */
+    public function company()
+    {
+        return $this->belongsTo(Company::class);
+    }
 }
